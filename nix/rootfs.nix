d6c24d125874fa6240bad860e897618218752923
PopCat19 <atsuo11111@gmail.com>
2025-06-09 17:28:16 -0400

    broken-patch

--- nix/rootfs.nix
+++ nix/rootfs.nix
@@ -1,33 +1,21 @@
 # ./nix/rootfs.nix
 { pkgs, drivers }:
 
-pkgs.buildFHSUserEnv {
+pkgs.buildFHSEnv {
   name = "shimboot-nixos-fhs-rootfs";
 
-  # List all the packages you want in your final system.
   targetPkgs = ps: with ps; [
-    # The patched systemd from our overlay
     systemd
-    # A basic desktop and login manager
-    (xfce.override {
-      withGtk3 = true;
-    })
+    xfce.xfwm4
     lightdm-gtk-greeter
-    # Basic command line tools
     bashInteractive
     coreutils
-    # Add whatever else you want here...
     neofetch
     firefox
   ];
 
-  # This runs after the environment is built to add extra files.
   extraCommands = ''
-    # Copy the drivers from our custom package into the FHS root.
     cp -r --no-preserve=ownership ${drivers}/lib/* $out/lib/
-
-    # Add other custom configs here, replacing the old `rootfs` dir.
-    # For example:
     mkdir -p $out/etc/modules-load.d
     echo "tun" > $out/etc/modules-load.d/tun.conf
   '';
