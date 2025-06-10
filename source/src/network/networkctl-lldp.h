43f120a8c20956fb2e5d74dee7c20c544fbe18d0
PopCat19 <atsuo11111@gmail.com>
2025-06-10 14:53:11 -0400

    update README, gitignore, remove unneeded /source

--- source/src/network/networkctl-lldp.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: LGPL-2.1-or-later */
-#pragma once
-
-#include "sd-varlink.h"
-
-#include "format-table.h"
-
-int dump_lldp_neighbors(sd_varlink *vl, Table *table, int ifindex);
-int link_lldp_status(int argc, char *argv[], void *userdata);
--- source/src/timesync/timesyncd-bus.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: LGPL-2.1-or-later */
-#pragma once
-
-#include "bus-object.h"
-#include "timesyncd-manager.h"
-
-extern const BusObjectImplementation manager_object;
-
-int manager_connect_bus(Manager *m);
